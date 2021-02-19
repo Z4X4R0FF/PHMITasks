6270f4dccba35e7146dbdf01a7174b37f43d8867
ForgefiendTaurus <kirillichz941@gmail.com>
2021-02-19 22:31:38 +0300

    Update

--- PHMITasks/Task1.cs
+++ PHMITasks/Task1.cs
@@ -14,9 +14,6 @@ namespace PHMITasks
             PrintMessage(ConsoleColor.White, "1 for ArabNumbersTest, 2 for CharTest");
             var task = Console.ReadKey();
             Console.Clear();
-            PrintMessage(ConsoleColor.White, "Write test count");
-            _testCount = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
             switch (task.Key)
             {
                 case ConsoleKey.D1:
@@ -38,26 +35,27 @@ namespace PHMITasks
             '\u2642', '\u2640',
             '\u266A', '\u266B',
         };
-        
+
         //Памятка пользователю
         private static readonly string CharDescription =
-            $"1 = {PrintableChars[0]} | 2 = {PrintableChars[1]} |" +
-            $" 3 = {PrintableChars[2]} | 4 = {PrintableChars[3]} |" +
-            $" 5 = {PrintableChars[4]} | 6 = {PrintableChars[5]} |" +
-            $" 11 = {PrintableChars[6]} | 12 = {PrintableChars[7]} |" +
-            $" 13 = {PrintableChars[8]} | 14 = {PrintableChars[9]}";
+            $"0 = {PrintableChars[0]} | 1 = {PrintableChars[1]} |" +
+            $" 2 = {PrintableChars[2]} | 3 = {PrintableChars[3]} |" +
+            $" 4 = {PrintableChars[4]} | 5 = {PrintableChars[5]} |" +
+            $" 6 = {PrintableChars[6]} | 7 = {PrintableChars[7]} |" +
+            $" 8 = {PrintableChars[8]} | 9 = {PrintableChars[9]}";
 
         //Список цифр для теста
-        private readonly List<int> _printableNumbers = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        private readonly List<int> _printableNumbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         //Список цветов консоли
-        private readonly Array _colors = Enum.GetValues(typeof(ConsoleColor));
+        private readonly ConsoleColor[] _colors = new ConsoleColor[3] { ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Yellow };
 
         //Количество чисел в тесте
-        private const int CharCount = 10;
+        private const int ArabNumbersCount = 10;
+        private const int CharCount = 5;
 
         //Количество тестов
-        private readonly int _testCount;
+        private const int TestCount = 3;
 
         //Пройденные тесты за сессию
         private readonly List<Tuple<string, string, ConsoleColor>> _tests =
@@ -76,7 +74,7 @@ namespace PHMITasks
             foreach (var (test, answer, color) in _tests)
             {
                 var correctNumbers = 0;
-                for (var i = 0; i < CharCount * 2; i += 2)
+                for (var i = 0; i < ArabNumbersCount; i ++)
                 {
                     //Сравниваем строки посимвольно
                     try
@@ -90,7 +88,7 @@ namespace PHMITasks
                 }
 
                 //Считаем процент выполнения теста
-                var result = correctNumbers / ((float) CharCount / 100);
+                var result = correctNumbers / ((float)ArabNumbersCount / 100);
                 _results.Add($"Arab Test With {color.ToString()} color result {result}%.");
                 PrintMessage(ConsoleColor.White, $"test result is {result}%.");
             }
@@ -104,12 +102,13 @@ namespace PHMITasks
             foreach (var (test, answer, color) in _tests)
             {
                 var correctChars = 0;
-                for (var i = 0; i < CharCount * 2; i += 2)
+                for (var i = 0; i < CharCount; i ++)
                 {
                     //Сравниваем строки посимвольно
                     try
                     {
-                        if (test[i] == answer[i]) correctChars++;
+                        var number = int.Parse(answer[i].ToString());
+                        if (test[i] == PrintableChars[number]) correctChars++;
                     }
                     catch (Exception)
                     {
@@ -118,7 +117,7 @@ namespace PHMITasks
                 }
 
                 //Считаем процент выполнения теста
-                var result = correctChars / ((float) CharCount / 100);
+                var result = correctChars / ((float)CharCount / 100);
                 _results.Add($"Char Test With {color.ToString()} color result {result}%.");
                 PrintMessage(ConsoleColor.White, $"test result is {result}%.");
             }
@@ -135,27 +134,28 @@ namespace PHMITasks
         {
             PrintMessage(
                 ConsoleColor.White,
-                $"You will see {CharCount} numbers {_testCount} times, try to remember and reproduce them. U have 10 seconds to remember");
+                $"You will see {ArabNumbersCount} numbers {TestCount} times, try to remember and reproduce them. U have 10 seconds to remember");
             var rnd = new Random();
-            for (var i = 0; i < _testCount; i++)
+            for (var i = 0; i < TestCount; i++)
             {
                 //Перемешиваем массив
                 var numbers = _printableNumbers.OrderBy(x => rnd.Next()).ToArray();
-                var color = (ConsoleColor) _colors.GetValue(rnd.Next(1, _colors.Length));
+                var color = _colors[i];
                 var print = "";
                 //Собираем строку теста
-                for (var j = 0; j < CharCount; j++)
+                for (var j = 0; j < ArabNumbersCount; j++)
                 {
-                    print += $"{numbers[j]},";
+                    print += $"{numbers[j]}";
                 }
 
                 PrintMessage(ConsoleColor.White, $"Test {i + 1}");
                 PrintMessage(color, print);
                 System.Threading.Thread.Sleep(10000);
                 Console.Clear();
-                PrintMessage(ConsoleColor.White, "Now repeat numbers, split with ',' ");
+                PrintMessage(ConsoleColor.White, "Now repeat numbers.");
                 //Получаем результат теста
                 var answer = Console.ReadLine();
+                Console.Clear();
                 _tests.Add(new Tuple<string, string, ConsoleColor>(print, answer, color));
             }
 
@@ -167,29 +167,30 @@ namespace PHMITasks
         {
             PrintMessage(
                 ConsoleColor.White,
-                $"You will see {CharCount} chars {_testCount} times, try to remember and reproduce them. U have 10 seconds to remember");
+                $"You will see {CharCount} chars {TestCount} times, try to remember and reproduce them. U have 10 seconds to remember");
             var rnd = new Random();
-            for (var i = 0; i < _testCount; i++)
+            for (var i = 0; i < TestCount; i++)
             {
                 //Перемешиваем массив
                 var chars = PrintableChars.OrderBy(x => rnd.Next()).ToArray();
-                var color = (ConsoleColor) _colors.GetValue(rnd.Next(1, _colors.Length));
+                var color = _colors[i];
                 var print = "";
                 //Собираем строку теста
                 for (var j = 0; j < CharCount; j++)
                 {
-                    print += $"{chars[j]},";
+                    print += $"{chars[j]}";
                 }
 
                 PrintMessage(ConsoleColor.White, $"Test {i + 1}");
                 PrintMessage(color, print);
                 System.Threading.Thread.Sleep(10000);
                 Console.Clear();
-                PrintMessage(ConsoleColor.White, "Now repeat numbers, split with ','. Use Alt+NumPad keycode");
+                PrintMessage(ConsoleColor.White, "Now repeat numbers.");
                 PrintMessage(ConsoleColor.White, CharDescription);
 
                 //Получаем результат теста
                 var answer = Console.ReadLine();
+                Console.Clear();
                 _tests.Add(new Tuple<string, string, ConsoleColor>(print, answer, color));
             }
 
@@ -209,11 +210,13 @@ namespace PHMITasks
         {
             using (var writer = File.AppendText("../../results.txt"))
             {
+                writer.WriteLine("-----------");
                 foreach (var result in _results)
                 {
                     writer.WriteLine(result);
                 }
             }
+            Console.Read();
         }
     }
 }
